ec2c57c689c80d44250cf4653141e2baf8b905d4
Morphyum <zion@c-schwabe.de>
2018-06-25 00:47:36 +0200

    * some work torwards the goal

--- MercDeployments/MercDeployments/HolderClasses.cs
+++ MercDeployments/MercDeployments/HolderClasses.cs
@@ -4,8 +4,29 @@ using System.Collections.Generic;
 
 namespace MercDeployments {
     public class Settings {
+        public float MissionChancePerDay = 0.1f;
     }
     
     public static class Fields {
+        public static bool Deployment = false;
+        public static Dictionary<string, Contract> DeploymentContracts = new Dictionary<string, Contract>();
+        public static Faction DeploymentEmployer = Faction.INVALID_UNSET;
+        public static Faction DeploymentTarget = Faction.INVALID_UNSET;
+        public static int DeploymentDifficulty = 1;
+
+    }
+
+    public struct PotentialContract {
+        // Token: 0x040089A4 RID: 35236
+        public ContractOverride contractOverride;
+
+        // Token: 0x040089A5 RID: 35237
+        public Faction employer;
+
+        // Token: 0x040089A6 RID: 35238
+        public Faction target;
+
+        // Token: 0x040089A7 RID: 35239
+        public int difficulty;
     }
 }
--- MercDeployments/MercDeployments/Patch.cs
+++ MercDeployments/MercDeployments/Patch.cs
@@ -5,13 +5,9 @@ using BattleTech.Save;
 using BattleTech.Save.SaveGameStructure;
 using BattleTech.UI;
 using Harmony;
-using HBS.Collections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using TMPro;
-using UnityEngine;
 
 namespace MercDeployments {
 
@@ -19,6 +15,91 @@ namespace MercDeployments {
     [HarmonyPatch(new Type[] { typeof(GameInstance), typeof(SaveReason) })]
     public static class GameInstanceSave_Constructor_Patch {
         static void Postfix(GameInstanceSave __instance) {
+            Helper.SaveState(__instance.InstanceGUID, __instance.SaveTime);
         }
-    } 
+    }
+
+    [HarmonyPatch(typeof(GameInstance), "Load")]
+    public static class GameInstance_Load_Patch {
+        static void Prefix(GameInstanceSave save) {
+            Helper.LoadState(save.InstanceGUID, save.SaveTime);
+        }
+    }
+
+    [HarmonyPatch(typeof(AAR_SalvageScreen), "OnCompleted")]
+    public static class AAR_SalvageScreen_OnCompleted_Patch {
+        static void Postfix(AAR_SalvageScreen __instance) {
+            try {
+                Contract con = (Contract)ReflectionHelper.GetPrivateField(__instance, "contract");
+                Fields.DeploymentContracts.Remove(con.Name);
+            } catch(Exception e) {
+                Logger.LogError(e);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "OnBreadcrumbArrival")]
+    public static class SimGameState_OnBreadcrumbArrival_Patch {
+        static void Postfix(SimGameState __instance) {
+            Fields.Deployment = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SGTimePlayPause), "ToggleTime")]
+    public static class SGTimePlayPause_ToggleTime_Patch {
+        static bool Prefix(SGTimePlayPause __instance) {
+            if (Fields.Deployment && Fields.DeploymentContracts.Count > 0) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "PrepareBreadcrumb")]
+    public static class SimGameState_PrepareBreadcrumb_Patch {
+        static void Postfix(SimGameState __instance, Contract contract) {
+            Fields.DeploymentContracts.Add(contract.Name, contract);
+            Fields.DeploymentDifficulty = contract.Difficulty;
+            Fields.DeploymentEmployer = contract.Override.employerTeam.faction;
+            Fields.DeploymentTarget = contract.Override.targetTeam.faction;
+        }
+    }
+    
+
+    [HarmonyPatch(typeof(SGRoomController_CmdCenter), "GetAllContracts")]
+    public static class SGRoomController_CmdCenter_GetAllContracts_Patch {
+        static bool Prefix(SGRoomController_CmdCenter __instance, ref List<Contract> __result) {
+            if (Fields.Deployment) {
+                List<Contract> list = Fields.DeploymentContracts.Values.ToList();
+                __result = list;
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "OnDayPassed")]
+    public static class SimGameState_OnDayPassed_Patch {
+        static void Postfix(SimGameState __instance) {
+            if (Fields.Deployment) {
+                Settings settings = Helper.LoadSettings();
+                Random rand = new Random();
+                if (rand.NextDouble() < settings.MissionChancePerDay) {
+                    __instance.PauseTimer();
+                    SimGameInterruptManager interruptQueue = (SimGameInterruptManager)AccessTools.Field(typeof(SimGameState), "interruptQueue").GetValue(__instance);
+                    Contract newcon = Helper.GetNewContract(__instance, Fields.DeploymentDifficulty, Fields.DeploymentEmployer, Fields.DeploymentTarget);
+                    Fields.DeploymentContracts.Add(newcon.Name, newcon);
+                    interruptQueue.QueueGenericPopup("New Mission", "Our Employer has a new mission for us.");
+                }
+            }
+        }
+
+
+
+    }
+    
 }
